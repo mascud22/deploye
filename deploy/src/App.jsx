b32a9743b568400694ea614664dd1eab92976de9
Mascud Yare <mascud22@gmail.com>
2026-01-01 13:38:52 +0300

    first commit

---
+++ deploy/src/App.jsx
@@ -0,0 +1,12 @@
+import { BriefcaseIcon } from '@heroicons/react/16/solid'
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      <button className=' bg-blue-500 flex items-center gap-2 font-semibold text-white rounded-3xl py-2 px-3.5'> <BriefcaseIcon className='size-5.5'/> it works yeaaaah</button>
+    </div>
+  )
+}
+
+export default App
