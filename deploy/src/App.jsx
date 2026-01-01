289e87dd45676fdcc8f13b8a288a39a1843f30b5
Mascud Yare <mascud22@gmail.com>
2026-01-01 15:32:06 +0300

    style changes easy

--- deploy/src/App.jsx
+++ deploy/src/App.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='flex justify-center items-center h-screen bg-black'>
-      <button className=' bg-white flex items-center gap-2 font-semibold text-black rounded-3xl py-2 px-3.5'> <BriefcaseIcon className='size-5.5'/> it works yeaaaah</button>
+    <div className='flex justify-center items-center h-screen bg-blue-400'>
+      <button className=' bg-blue-500 flex items-center gap-2 font-semibold text-black rounded-3xl py-2 px-3.5'> <BriefcaseIcon className='size-5.5'/> it works yeaaaah</button>
     </div>
   )
 }
