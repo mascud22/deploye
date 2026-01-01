d3989d632fe240e3a341aa585d369ec0b9858130
Mascud Yare <mascud22@gmail.com>
2026-01-01 15:45:20 +0300

    new styles new look

--- deploy/src/App.jsx
+++ deploy/src/App.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='flex justify-center items-center h-screen bg-blue-400'>
-      <button className=' bg-blue-500 flex items-center gap-2 font-semibold text-black rounded-3xl py-2 px-3.5'> <BriefcaseIcon className='size-5.5'/> it works yeaaaah</button>
+    <div className='flex justify-center items-center h-screen bg-black flex-col gap-2'>
+      <button className=' bg-white flex items-center gap-2 font-semibold text-black rounded-3xl py-2 px-3.5'> <BriefcaseIcon className='size-5.5'/> it works yeaaaah</button>
+      <p className='text-neutral-300 text-center w-65'>For sure we win and also take care your team man</p>
     </div>
   )
 }
